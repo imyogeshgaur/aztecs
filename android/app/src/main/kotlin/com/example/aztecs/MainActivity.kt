c4270fc9d9e739f05a1c70d049ee9e86916b481c
coderbanda <yogeshgaur1995@gmail.com>
2020-04-18 12:23:36 +0530

    fourth commit

---
+++ android/app/src/main/kotlin/com/example/aztecs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aztecs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,313 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp
+    (
+      debugShowCheckedModeBanner: false,
+      home:Scaffold
+      (
+           backgroundColor:Colors.purple.shade200,
+          appBar: AppBar
+          (
+            backgroundColor: Colors.red.shade300,
+            title:Text("Team Aztecs India"),
+            centerTitle: true,
+          ),
+        body:SafeArea
+        (
+        
+          child: Column
+          (
+            children: <Widget>
+            [
+              CircleAvatar
+              (
+                    backgroundImage: AssetImage('images/yogesh.jpg'),
+                    radius:30.0,
+            
+              ),
+              Text
+              (
+                "Web and App Developer",
+                style:TextStyle
+                (
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  color:Colors.white,
+                ),
+              ),
+               Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.phone,
+                               ),
+                               title: Text
+                               (
+                                 "+91 98687 47636",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+              Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.email,
+                               ),
+                               title: Text
+                               (
+                                 "yogesh.gaur@aztecsindia.com",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+            CircleAvatar
+            (
+              radius: 30.0,
+              backgroundImage: AssetImage("images/aman.jpeg"),
+             ),
+              Text
+              (
+                "communication and networking Engineer",
+                 style:TextStyle
+                   (
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color:Colors.white,
+                        ),
+                      ),
+                     Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.phone,
+                               ),
+                               title: Text
+                               (
+                                 "+91 12345 67890",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+                        Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.email,
+                               ),
+                               title: Text
+                               (
+                                 "aman.solanki@aztecsindia.com",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+                       CircleAvatar
+                     (
+                        radius: 30.0,
+                        backgroundImage: AssetImage("images/shrey.jpeg"),
+                     ),
+                      Text
+                      (
+                         "Data Science and Automation Engineer",
+                         style:TextStyle
+                         (
+                           fontSize: 15.0,
+                           fontWeight: FontWeight.bold,
+                           color:Colors.white,
+                         )
+                      ),
+                        Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.phone,
+                               ),
+                               title: Text
+                               (
+                                 "+91 98765 54321",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+             Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.email,
+                               ),
+                               title: Text
+                               (
+                                 "shrey.midha@aztecsindia.com",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+                    CircleAvatar
+                    (
+                      backgroundImage: AssetImage('images/himanshu.jpg'),
+                      radius:30.0,
+                    ),
+                    Text
+                    (
+                      "Hardware and chipset Engineer",
+                      style:TextStyle
+                      (
+                         fontSize:15.0,
+                         fontWeight: FontWeight.bold,
+                         color:Colors.white,
+                      ),
+                    ),
+                    Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.phone,
+                               ),
+                               title: Text
+                               (
+                                 "+91 98751 54345",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+                Container
+                      (
+                        child:Card
+                        (
+                           color:Colors.white,
+                           margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                           child:Padding
+                           (
+                             padding:EdgeInsets.all(1.0),
+                             child:ListTile(
+                               leading: Icon(
+                                  Icons.email,
+                               ),
+                               title: Text
+                               (
+                                 "himanshu.rastogi@aztecsindia.com",
+                                 style: TextStyle
+                                (
+                                  color:Colors.purple.shade900,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                             )
+                           ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ), 
+              ),
+            );
+         }
+}
